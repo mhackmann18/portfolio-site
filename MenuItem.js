e0ef588c296d159277236e34f3f8bd31a5499db1
Matt Hackmann <mhackmann13@icloud.com>
2022-03-22 16:16:42 -0500

    Moved MenuItem class to its own file

---
+++ MenuItem.js
@@ -0,0 +1,47 @@
+class MenuItem {
+  #headerEl;
+  #contentWrapperEl;
+  #expanded;
+
+  constructor(headerEl, contentWrapperEl){
+    this.#headerEl = headerEl;
+    this.#contentWrapperEl = contentWrapperEl;
+    this.#expanded = false;
+    this.init();
+  }
+
+  init(){
+    this.#headerEl.onclick = () => {
+      if(!this.#expanded){
+    
+        this.#headerEl.classList.remove("text-orange-animated");
+    
+        for(let className of this.#headerEl.classList){
+          if(className === "text-orange"){
+            this.#headerEl.classList.remove("text-orange");
+            this.#headerEl.classList.add("text-black");
+          }
+        }
+    
+        this.#headerEl.classList.add("translate");
+    
+        this.#contentWrapperEl.style.height = "75vh";
+        this.#expanded = true;
+      } else {
+        this.#headerEl.classList.add("text-orange-animated");
+    
+        for(let className of this.#headerEl.classList){
+          if(className === "text-black"){
+            this.#headerEl.classList.remove("text-black");
+            this.#headerEl.classList.add("text-orange");
+          }
+        }
+    
+        this.#headerEl.classList.remove("translate");
+    
+        this.#contentWrapperEl.style.height = "0vh";
+        this.#expanded = false;
+      }
+    }
+  }
+}
--- script.js
+++ script.js
@@ -2,61 +2,6 @@
 
 let sectionExpanded = false;
 const projectsTitle = document.getElementById("projects-title");
+const contentWrapper = document.getElementsByClassName("content-wrapper");
 
-class MenuItem {
-  constructor(headerEl, contentWrapperEl, content){
-    this.headerEl = headerEl;
-    this.contentWrapperEl = contentWrapperEl;
-    this.content = content;
-  }
-
-
-}
-
-projectsTitle.onclick = e => {
-  if(!sectionExpanded){
-    let header = e.target;
-
-    header.classList.remove("text-orange-animated");
-
-    for(let className of header.classList){
-      if(className === "text-orange"){
-        header.classList.remove("text-orange");
-        header.classList.add("text-black");
-      }
-    }
-
-    header.classList.add("translate");
-
-    let el = header.parentElement.nextSibling;
-
-    while(!(el.className === "content-wrapper")){
-      el = el.nextSibling;
-    }
-
-    el.style.height = "75vh";
-    sectionExpanded = true;
-  } else {
-    let header = e.target;
-
-    header.classList.add("text-orange-animated");
-
-    for(let className of header.classList){
-      if(className === "text-black"){
-        header.classList.remove("text-black");
-        header.classList.add("text-orange");
-      }
-    }
-
-    header.classList.remove("translate");
-
-    let el = header.parentElement.nextSibling;
-
-    while(!(el.className === "content-wrapper")){
-      el = el.nextSibling;
-    }
-
-    el.style.height = "0vh";
-    sectionExpanded = false;
-  }
-}
+new MenuItem(projectsTitle, contentWrapper[0]);
